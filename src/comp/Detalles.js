9e31d5290bf596548cc6fe90482aeefab7636874
lchaconc <luis.chacon.campos@gmail.com>
2020-04-26 11:34:17 -0600

    formato

--- src/comp/Detalles.js
+++ src/comp/Detalles.js
@@ -8,33 +8,46 @@ function Detalles(props) {
             <div className="row">
                 <div className="col-sm-12 text-center">
                     <h3>{item.nombre}</h3>
-                </div>
+                    <p className="item-detalles">
+                        <span className="badge badge-info">
+                            Categoría: {item.nombreTipo}
+                        </span> 
+                    </p>
+                </div>                
             </div>
+            <hr/>
             <div className="row">
                 <div className="col-sm-12">
-                <h4>
+                <p className="item-detalles">
                     <span className="badge badge-info">
-                        Nombre del contacto: {item.contacto}
-                    </span>                
-                </h4>
-                <h4>
+                        Contacto: 
+                    </span> {item.contacto}                
+                </p>
+                <p className="item-detalles">
+                    <span className="badge badge-info"> 
+                    Ubicación: 
+                    </span> {item.ubicacion}
+                </p>
+                <p className="item-detalles">
                     <span className="badge badge-info">
-                        Ubicación: {item.ubicacion}
-                    </span>                
-                </h4>
-                <h4>
+                        Horario: 
+                    </span> {item.horario}                
+                </p>
+                <p className="item-detalles">
                     <span className="badge badge-info">
-                        Horario: {item.horario}
-                    </span>                
-                </h4>
-                <h4>
+                        Teléfono: 
+                    </span> {item.telefono1}         
+                </p>
+                <p className="item-detalles">
                     <span className="badge badge-info">
-                        Teléfono: {item.telefono1}
-                    </span>                
-                </h4>
-                <a href={item.face} target="_blank" rel="noopener noreferrer" >
+                        Facebook: 
+                    </span>
+                    <a href={item.face} target="_blank" rel="noopener noreferrer" >
                     {item.face}
                 </a>
+                </p>
+
+
                 <br/>
                 <div className="alert alert-secondary" role="alert">
                     {item.descripcion}
@@ -46,9 +59,9 @@ function Detalles(props) {
                 <div className="col-sm-12 text-center">
                     {
                         item.activo === "1" ?
-                            <i className="fas fa-toggle-on text-success"></i>                                
+                            <i className="fas fa-toggle-on text-success icono-2"></i>                                
                         :
-                            <i className="fas fa-toggle-off"></i>
+                            <i className="fas fa-toggle-off icono-2" ></i>
                     }
 
                 </div>
